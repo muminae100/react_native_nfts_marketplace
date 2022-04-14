59e4271a09fdaecbcf60fa569a1bd535e438bdce
foreverkenyan <smuminaetx100@gmail.com>
2022-04-14 17:54:42 +0300

    Add a place bid button

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -55,7 +55,11 @@ const NFTCard = ({ data }) => {
           }}
         >
           <EthPrice price={data.price} />
-          <RectButton />
+          <RectButton
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate("Details", { data })}
+          />
         </View>
       </View>
     </View>
