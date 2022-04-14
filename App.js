476120088086524fa3ab668bf2afd2284aa0fc48
foreverkenyan <smuminaetx100@gmail.com>
2022-04-14 14:35:34 +0300

    Install all required dependencies for the project

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My first React NATIVE APP!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello world!</Text>
     </View>
   );
 }
